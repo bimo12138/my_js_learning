4477cf8a1a3c5828a021d88e4629cea9713e2dba
bimo <1371639183@qq.com>
2019-06-26 15:18:38 +0800

    start vue

---
+++ src/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-menu :default-active="defaultIndex" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+    <el-menu-item index="0">首页</el-menu-item>
+    <el-submenu index='1'>
+        <template slot="title">
+            个性选项
+        </template>
+        <el-menu-item index='1-1'>选项一</el-menu-item>
+        <el-menu-item index='1-2'>选项二</el-menu-item>
+    </el-submenu>
+
+        <el-menu-item index='2'>注册</el-menu-item>
+        <el-menu-item index='3'>登录</el-menu-item>
+
+</el-menu>
+</template>
+
+
+<style>
+    .right_group {
+        float: right;
+    }
+    .right_group li {
+        display: inline;
+    }
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+                defaultIndex: '0'
+            }
+        }
+    }
+</script>
