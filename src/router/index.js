9890cc7a806e261c5f2310e1caac94f195fc8908
bimo <1371639183@qq.com>
2019-08-25 21:19:15 +0800

    change vue-router index ways

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import index from "../components/index.vue";
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import login from "../components/login.vue";
-import register from "../components/register.vue";
-import rank from '../components/rank.vue';
-import student_info from "../components/student_pages/student_info.vue";
-import student from "../components/student_pages/student.vue";
-import student_take_exam from "../components/student_pages/student_take_exam.vue";
-import student_grade from "../components/student_pages/student_grade.vue";
-import teacher from "../components/teacher_pages/teacher.vue"
-import teacher_login from "../components/teacher_pages/teacher_login.vue"
-import teacher_info from "../components/teacher_pages/teacher_info.vue";
-import teacher_register from "../components/teacher_pages/teacher_register.vue";
-import demo_exam from "../components/teacher_pages/demo_exam.vue";
-import upload_exam from "../components/teacher_pages/upload_exam.vue";
-import my_exam from "../components/teacher_pages/my_exam.vue";
-import exam_detail from "../components/teacher_pages/exam_detail.vue";
-import student_exam from "../components/student_pages/student_exam.vue";
+
 
 
 Vue.use(Router)
@@ -29,73 +14,89 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: index,
+      component: resolve => require(["../components/index.vue"], resolve),
       name: "index"
-    }, {
+    }, 
+    {
       path: "/login",
-      component: login,
+      component: resolve => require(["../components/login.vue"], resolve),
       name: "login"
-    }, {
+    }, 
+    {
       path: "/register",
-      component: register,
+      component: resolve => require(["../components/register.vue"], resolve),
       name: "register"
-    }, {
+    }, 
+    {
       path: "/rank",
-      component: rank,
+      component: resolve => require(["../components/rank.vue"], resolve),
       name: "rank"
-    }, {
+    },
+    {
       path: "/student",
-      component: student,
+      component: resolve => require(["../components/student_pages/student.vue"], resolve),
       name: "student",
-      children: [{
+      children: [
+      {
         path: "info",
-        component: student_info,
+        component: resolve => require(["../components/student_pages/student_info.vue"], resolve),
         name: "student_info"
-      }, {
+      },
+      {
         path: "exam",
-        component: student_take_exam,
+        component: resolve => require(["../components/student_pages/student_exam.vue"], resolve),
         name: "student_take_exam"
-      }, {
+      }, 
+      {
         path: "grade",
-        component: student_grade,
+        component: resolve => require(["../components/student_pages/student_grade.vue"], resolve),
         name: "student_grade"
-      }, {
+      }, 
+      {
         path: "exam/:exam_no",
-        component: student_exam,
+        component: resolve => require(["../components/student_pages/student_take_exam.vue"], resolve),
         name: "student_exam"
       }]
-    }, {
+    }, 
+    {
       path: "/teacher",
-      component: teacher,
+      component: resolve => require(["../components/teacher_pages/teacher.vue"], resolve),
       name: "teacher",
-      children: [{
+      children: [
+      {
         path: "info",
-        component: teacher_info,
+        component: resolve => require(["../components/teacher_pages/teacher_info.vue"], resolve),
         name: "teacher_info"
-      }, {
+      }, 
+      {
         path: "demo_exam",
-        component: demo_exam,
+        component: resolve => require(["../components/teacher_pages/demo_exam.vue"], resolve),
         name: "demo_exam"
-      }, {
+      }, 
+      {
         path: "upload_exam",
-        component: upload_exam,
+        component: resolve => require(["../components/teacher_pages/upload_exam.vue"], resolve),
         name: "upload_exam"
-      }, {
+      }, 
+      {
         path: "my_exam",
-        component: my_exam,
+        component: resolve => require(["../components/teacher_pages/my_exam.vue"], resolve),
         name: "my_exam"
-      }, {
+      }, 
+      {
         path: "exam/:exam_no",
-        component: exam_detail,
+        component: resolve => require(["../components/teacher_pages/exam_detail.vue"], resolve),
         name: "exam_detail"
       }]
-    }, {
+    }, 
+    {
       path: "/teacher_login",
-      component: teacher_login,
+      component: resolve => require(["../components/teacher_pages/teacher_login.vue"], resolve),
       name: "teacher_login"
-    }, {
+    }, 
+    {
       path: "/teacher_register",
-      component: teacher_register,
+      component: resolve => require(["../components/teacher_pages/teacher_register.vue"], resolve),
       name: "teacher_register"
     }
   ]
